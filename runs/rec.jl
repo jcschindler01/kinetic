752925c877375fdd87d7903a8e0a0abed040668e
Joseph Schindler <jcschindler01@gmail.com>
2024-04-02 12:21:09 +0200

    Updated filenames.

--- runs/rec.jl
+++ runs/rec.jl
@@ -16,5 +16,5 @@ record_animation("txt/gun_naive_N500_T4_1707926341.txt", save=true)
 # "chain_naive_N1000_T10_1707918727"
 # "corner_naive_N500_T4_1707916201"
 # "corner_free_N500_T4_1708531699"
-
-
+# "hotcold_naive_N500_T1_1711556349"
+# "gun_naive_N500_T4_1707926341"
